96d6de353154ef8c602b01aec83dc68e2b03dbd9
akhona <akhonam850@gmail.com>
2025-02-08 20:45:02 +0200

    configs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { div } from 'three/tsl'
+ 
+const App = ()=>{
+  return (
+   <div>
+    hello world
+   </div>
+  )
+}
+
+export default App
